a97cd88e017adf6322919ee555a576fb658bb228
tomsteuten <tom.steuten@gmail.com>
2025-12-10 13:24:21 +1000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "ns-parser-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest"
+  // add icon files here if you add them later:
+  // "./icon-192.png",
+  // "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((k) => k !== CACHE_NAME)
+          .map((k) => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
+  );
+});
